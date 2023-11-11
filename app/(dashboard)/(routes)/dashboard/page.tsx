a5a23269c62ca5073382153aa66a942e4af98450
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-11-11 14:16:45 +0700

    Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,3 @@
-import { UserButton } from '@clerk/nextjs';
-
 export default function Home() {
-  return (
-    <div>
-      Home page
-      <div>
-        <UserButton afterSignOutUrl='/' />
-      </div>
-    </div>
-  );
+  return <div>Home page</div>;
 }
